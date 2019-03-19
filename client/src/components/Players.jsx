132b7ad2dd6c0b1ff4bc6ae8008e82b97cbb00a0
badwolf <choa.marc@gmail.com>
2019-03-19 14:59:04 -0400

    Player cards with content of name & score
    - rendered player cards displaying name and score
    - creating states with name and score

--- client/src/components/Players.jsx
+++ client/src/components/Players.jsx
@@ -1,31 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 class Players extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      names: ['orange', 'blurple']
-
+      names: ["orange", "blurple"],
+      score: 0
     };
-
   }
-  componentDidMount() {
-    
-  }
-
-  
+  componentDidMount() {}
 
   render() {
     return (
-      <div className="row">
-        <div>
-          {this.state.names.map((name) => (
-            <li>{name}</li>
+        <div className="row justify-content-md-center">
+          {this.state.names.map((name, i) => (
+            <div className="col-4 card" key={i}>
+              <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Score: {this.state.score}</h6>
+              </div>
+            </div>
           ))}
         </div>
-      </div>
     );
   }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -19,14 +19,15 @@ class App extends React.Component {
     };
     this.getData = this.getData.bind(this);
     this.players_choice = this.players_choice.bind(this);
-    this.layout_reset = this.layout_reset.bind(this)
+    this.layout_reset = this.layout_reset.bind(this);
   }
   componentDidMount() {
     this.getData();
   }
 
   getData() {
-    axios.get(`/question`)
+    axios
+      .get(`/question`)
       .then(({ data }) => {
         console.log(data);
         this.setState({
@@ -41,33 +42,34 @@ class App extends React.Component {
       .catch(err => console.error(err));
   }
 
-  players_choice(e){
+  players_choice(e) {
     e.preventDefault();
-    if(e.target.dataset.choice === this.state.correct_answer){
+    if (e.target.dataset.choice === this.state.correct_answer) {
       // add points based on difficulty to current player
       // point system (ADD)
-        // easy - 200pts
-        // medium - 400pts
-        // hard - 800pts
-      this.setState({button_color: "btn-success"})
-      
+      // easy - 200pts
+      // medium - 400pts
+      // hard - 800pts
+      this.setState({ button_color: "btn-success" });
     } else {
       // negate points based on difficulty to current player
       // point system (NEGATE)
-        // easy - 200pts
-        // medium - 400pts
-        // hard - 800pts
-      this.setState({button_color: "btn-danger"})
+      // easy - 200pts
+      // medium - 400pts
+      // hard - 800pts
+      this.setState({ button_color: "btn-danger" });
     }
-    
+
     // after click switch to next player && request new question
-    setTimeout(() => {this.getData(), this.layout_reset()}, 1000) //currently changing questions, next is players
+    setTimeout(() => {
+      this.getData(), this.layout_reset();
+    }, 1000); //currently changing questions, next is players
   }
 
-  layout_reset(){
+  layout_reset() {
     this.setState({
       button_color: "btn-light"
-    })
+    });
   }
 
   render() {
@@ -79,18 +81,22 @@ class App extends React.Component {
         <div className="main">
           <section className="jumbotron text-center trivia-field">
             <div className="container">
-              <div className="jumbotron-heading">
+              <div className="jumbotron-heading large-question">
                 <TriviaQuestion main_question={this.state.question} />
               </div>
 
               <div className="options">
-                <TriviaChoices options={this.state.choices} chosen={this.players_choice} btn_color={this.state.button_color}/>
+                <TriviaChoices
+                  options={this.state.choices}
+                  chosen={this.players_choice}
+                  btn_color={this.state.button_color}
+                />
               </div>
             </div>
           </section>
-          <div className="container">
-            <Players question_level={this.state.difficulty}/>
-          </div>
+        </div>
+        <div className="container">
+          <Players question_level={this.state.difficulty} />
         </div>
       </Fragment>
     );
