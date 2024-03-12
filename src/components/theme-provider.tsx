8ba161871960680682e49de29123a04064ca9cee
badwolf <choa.marc@gmail.com>
2024-03-11 20:15:00 -0400

    chore: remove comments TODO from theme-provider.tsx
    - installed next.js theme dependencies

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-// TODO: pnpm add next-themes
-
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
